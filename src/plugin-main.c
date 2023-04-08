d2bef8354dbcc82368b10eb2a42c80619e446ff9
cg2121 <claytong1214@gmail.com>
2023-04-08 17:17:07 -0500

    Revert "Update to use new video mix api"
    This reverts commit 2bcf81047d71dfd02d28df94afe96fd899c7dbd9.
    
    Revert for now, as it causes crashes.

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,11 +1,17 @@
-#include "obs-module.h"
+#include <obs-module.h>
+#include "plugin-macros.generated.h"
+#include <obs.h>
+#include <media-io/video-io.h>
+#include <media-io/video-frame.h>
+#include <obs-frontend-api.h>
 
 struct decklink_output_filter_context {
 	obs_output_t *output;
 	obs_source_t *source;
 
-	video_t *video;
-	obs_view_t *view;
+	video_t *video_output;
+	gs_texrender_t *texrender;
+	gs_stagesurf_t *stagesurface;
 
 	bool active;
 };
@@ -16,6 +22,60 @@ static const char *decklink_output_filter_get_name(void *unused)
 	return obs_module_text("DecklinkOutput");
 }
 
+static void render_preview_source(void *data, uint32_t cx, uint32_t cy)
+{
+	UNUSED_PARAMETER(cx);
+	UNUSED_PARAMETER(cy);
+
+	struct decklink_output_filter_context *filter = data;
+
+	uint32_t width = gs_stagesurface_get_width(filter->stagesurface);
+	uint32_t height = gs_stagesurface_get_height(filter->stagesurface);
+
+	if (!gs_texrender_begin(filter->texrender, width, height))
+		return;
+
+	struct vec4 background;
+	vec4_zero(&background);
+
+	gs_clear(GS_CLEAR_COLOR, &background, 0.0f, 0);
+	gs_ortho(0.0f, (float)width, 0.0f, (float)height, -100.0f, 100.0f);
+
+	gs_blend_state_push();
+	gs_blend_function(GS_BLEND_ONE, GS_BLEND_ZERO);
+
+	obs_source_skip_video_filter(filter->source);
+
+	gs_blend_state_pop();
+	gs_texrender_end(filter->texrender);
+
+	struct video_frame output_frame;
+	if (!video_output_lock_frame(filter->video_output, &output_frame, 1,
+				     obs_get_video_frame_time()))
+		return;
+
+	gs_stage_texture(filter->stagesurface,
+			 gs_texrender_get_texture(filter->texrender));
+
+	uint8_t *video_data;
+	uint32_t video_linesize;
+	if (!gs_stagesurface_map(filter->stagesurface, &video_data,
+				 &video_linesize))
+		return;
+
+	uint32_t linesize = output_frame.linesize[0];
+
+	for (uint32_t i = 0; i < height; i++) {
+		uint32_t dst_offset = linesize * i;
+		uint32_t src_offset = video_linesize * i;
+		memcpy(output_frame.data[0] + dst_offset,
+		       video_data + src_offset, linesize);
+	}
+
+	gs_stagesurface_unmap(filter->stagesurface);
+	video_output_unlock_frame(filter->video_output);
+}
+
 static void decklink_output_filter_stop(void *data)
 {
 	struct decklink_output_filter_context *filter = data;
@@ -23,12 +83,17 @@ static void decklink_output_filter_stop(void *data)
 	if (!filter->active)
 		return;
 
-	obs_view_remove(filter->view);
-	obs_view_set_source(filter->view, 0, NULL);
-	video_output_close(filter->video);
+	obs_remove_main_render_callback(render_preview_source, filter);
 
 	obs_output_stop(filter->output);
 	obs_output_release(filter->output);
+
+	obs_enter_graphics();
+	gs_stagesurface_destroy(filter->stagesurface);
+	gs_texrender_destroy(filter->texrender);
+	obs_leave_graphics();
+
+	video_output_close(filter->video_output);
 
 	filter->active = false;
 }
@@ -45,6 +110,13 @@ static void decklink_output_filter_start(void *data)
 	if (!obs_source_enabled(filter->source))
 		return;
 
+	obs_source_t *parent = obs_filter_get_target(filter->source);
+	uint32_t width = obs_source_get_base_width(parent);
+	uint32_t height = obs_source_get_base_height(parent);
+
+	if (!width || !height)
+		return;
+
 	obs_data_t *settings = obs_source_get_settings(filter->source);
 	const char *hash = obs_data_get_string(settings, "device_hash");
 	int mode_id = (int)obs_data_get_int(settings, "mode_id");
@@ -53,25 +125,39 @@ static void decklink_output_filter_start(void *data)
 	if (!hash || !*hash || !mode_id)
 		return;
 
-	if (!filter->view)
-		filter->view = obs_view_create();
-
-	obs_view_set_source(filter->view, 0,
-			    obs_filter_get_parent(filter->source));
-
-	if (!filter->video)
-		filter->video = obs_view_add(filter->view);
-
 	filter->output = obs_output_create(
 		"decklink_output", "decklink_filter_output", settings, NULL);
 
-	obs_output_set_media(filter->output, filter->video, obs_get_audio());
+	obs_enter_graphics();
+	filter->texrender = gs_texrender_create(GS_BGRA, GS_ZS_NONE);
+	filter->stagesurface = gs_stagesurface_create(width, height, GS_BGRA);
+	obs_leave_graphics();
+
+	const struct video_output_info *main_voi =
+		video_output_get_info(obs_get_video());
+
+	struct video_output_info vi = {0};
+	vi.format = VIDEO_FORMAT_BGRA;
+	vi.width = width;
+	vi.height = height;
+	vi.fps_den = main_voi->fps_den;
+	vi.fps_num = main_voi->fps_num;
+	vi.cache_size = 16;
+	vi.colorspace = main_voi->colorspace;
+	vi.range = main_voi->range;
+	vi.name = obs_source_get_name(filter->source);
+
+	video_output_open(&filter->video_output, &vi);
+	obs_output_set_media(filter->output, filter->video_output,
+			     obs_get_audio());
 
 	bool started = obs_output_start(filter->output);
 	filter->active = true;
 
 	if (!started)
 		decklink_output_filter_stop(filter);
+	else
+		obs_add_main_render_callback(render_preview_source, filter);
 }
 
 static void decklink_output_filter_update(void *data, obs_data_t *settings)
@@ -95,9 +181,24 @@ static void set_filter_enabled(void *data, calldata_t *calldata)
 		decklink_output_filter_stop(filter);
 }
 
+static void frontend_event(enum obs_frontend_event event, void *data)
+{
+	struct decklink_output_filter_context *filter = data;
+
+	switch (event) {
+	case OBS_FRONTEND_EVENT_FINISHED_LOADING:
+		decklink_output_filter_start(filter);
+		break;
+	default:
+		break;
+	}
+}
+
 static void *decklink_output_filter_create(obs_data_t *settings,
 					   obs_source_t *source)
 {
+	UNUSED_PARAMETER(settings);
+
 	struct decklink_output_filter_context *filter =
 		bzalloc(sizeof(struct decklink_output_filter_context));
 	filter->source = source;
@@ -106,16 +207,16 @@ static void *decklink_output_filter_create(obs_data_t *settings,
 	signal_handler_t *sh = obs_source_get_signal_handler(filter->source);
 	signal_handler_connect(sh, "enable", set_filter_enabled, filter);
 
-	decklink_output_filter_update(source, settings);
+	obs_frontend_add_event_callback(frontend_event, filter);
 	return filter;
 }
 
 static void decklink_output_filter_destroy(void *data)
 {
 	struct decklink_output_filter_context *filter = data;
-
+	obs_frontend_remove_event_callback(frontend_event, filter);
 	decklink_output_filter_stop(filter);
-	obs_view_destroy(filter->view);
+
 	bfree(filter);
 }
 
@@ -130,6 +231,16 @@ static obs_properties_t *decklink_output_filter_properties(void *data)
 	return props;
 }
 
+void decklink_output_filter_tick(void *data, float sec)
+{
+	struct decklink_output_filter_context *filter = data;
+
+	if (filter->texrender)
+		gs_texrender_reset(filter->texrender);
+
+	UNUSED_PARAMETER(sec);
+}
+
 struct obs_source_info decklink_output_filter = {
 	.id = "decklink_output_filter",
 	.type = OBS_SOURCE_TYPE_FILTER,
@@ -139,7 +250,7 @@ struct obs_source_info decklink_output_filter = {
 	.destroy = decklink_output_filter_destroy,
 	.update = decklink_output_filter_update,
 	.get_properties = decklink_output_filter_properties,
-};
+	.video_tick = decklink_output_filter_tick};
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("decklink-output-filter", "en-US")
